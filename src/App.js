cca1aef0c4492fc79946231e1b18dad5af5a8705
zakaria-akash <zakaria.93@yahoo.com>
2023-03-09 11:13:00 +0600

    adding authentication route with authentication component; implementing switchable functionality for login and signup depending on the user loging status; adjusting the custom form hook for switching between login and sign-up mode

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Authentication from "./user/pages/authentication";
 
 function App() {
   return (
@@ -16,8 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<User />} />
           <Route path="places/new" element={<NewPlace />} />
-          <Route path="/:userId/places" element={<UserPlaces />} />
+          <Route path=":userId/places" element={<UserPlaces />} />
           <Route path="places/:placeId" element={<UpdatePlace />} />
+          <Route path="auth" element={<Authentication />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </main>
---
+++ src/user/pages/authentication.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import CustomButton from "../../shared/components/FormElements/CustomButton";
+import NewPlaceInputForm from "../../shared/components/FormElements/NewPlaceInputForm";
+import Card from "../../shared/components/UIElements/Card";
+import { useCustomFormHook } from "../../shared/custom-hooks/custom-form-hook";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/utils/Validators";
+
+import classes from "./authentication.module.css";
+
+const Authentication = () => {
+  const [isLogInMode, setIsLogInMode] = useState(true);
+  const [formState, inputHandler, setFormData] = useCustomFormHook(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const authSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(formState.inputs);
+    if (formState.isValid) {
+      alert("Given data has been submitted!"); //will send this data to backend server
+    }
+  };
+
+  const switchModeHandler = () => {
+    if (!isLogInMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: "",
+            isValid: false,
+          },
+        },
+        false
+      );
+    }
+    setIsLogInMode((preVal) => !preVal);
+  };
+
+  return (
+    <Card className={classes.authentication}>
+      <h2>Login Required!</h2>
+      <hr />
+      <form id="authForm" onSubmit={authSubmitHandler}>
+        {!isLogInMode && (
+          <NewPlaceInputForm
+            element="input"
+            id="name"
+            type="text"
+            label="Your Name"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter your name"
+            onInput={inputHandler}
+          />
+        )}
+        <NewPlaceInputForm
+          element="input"
+          id="email"
+          type="email"
+          label="Email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address."
+          onInput={inputHandler}
+        />
+
+        <NewPlaceInputForm
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid password at least 5 characters."
+          onInput={inputHandler}
+        />
+
+        <CustomButton danger type="submit" disabled={!formState.isValid}>
+          {isLogInMode ? "LOGIN" : "SIGNUP"}
+        </CustomButton>
+      </form>
+      <CustomButton inverse onClick={switchModeHandler}>
+        {isLogInMode ? "SIGN UP" : "LOGIN"}
+      </CustomButton>
+    </Card>
+  );
+};
+
+export default Authentication;
